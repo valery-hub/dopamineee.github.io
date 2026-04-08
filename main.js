c19bece322b730112048c2e8690f5d393c8272ed
valery-hub <vaniavniaa7@gmail.com>
2026-04-08 23:15:49 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
 onload = () =>{
         document.body.classList.remove("container");
 };
+window.addEventListener("load", function() {
+  const audio = document.querySelector("audio");
+  audio.play().catch(() => {
+    document.body.addEventListener("click", () => {
+      audio.play();
+    });
+  });
+});
